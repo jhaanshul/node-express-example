3f5ea388a3b0ea21a543c788a569854daab16df4
Anshul Jha <anshul18032@iiitd.ac.in>
2020-04-10 10:17:40 +0530

    adding api endpoints for /dishes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const hostname = 'localhost';
 const port = '3000';
@@ -9,6 +9,50 @@ const app = express();//our APPLICATION IS GOING TO USE EXPRESS APP MODULE
 app.use(morgan('dev'));//use dev type for printing additional info
 
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next)=>{
+ res.statusCode = 200;
+ res.setHeader('Content-type','text');
+ next();//it will continue to look for additional specifications down below
+});
+
+app.get('/dishes',(req, res, next) =>{
+  res.end('will send all the dishes to you!');
+});
+
+app.post('/dishes',(req, res, next) => {
+  res.end('will add the information: '+ req.body.name+ ' with details ' + req.body.description);
+})
+
+app.put('/dishes',(req, res, next)=>{
+  res.statusCode = 403;	
+  res.end('put operation not supported on dishes');
+});
+
+app.delete('/dishes',(req, res, next)=>{
+  res.end('Deleting all the dishes!!');
+});
+
+app.get('/dishes/:dishId',(req, res, next) =>{
+  res.end('will send the dish'+req.params.dishId+' to you!');
+});
+
+
+app.put('/dishes/:dishId',(req, res, next)=>{
+  res.write('updating the dish '+req.params.dishId);
+  res.end('will update the dish '+ req.body.name+' with details '+ req.body.description);
+});
+
+app.post('/dishes/:dishId',(req, res, next) => {
+  res.statusCode = 403;	
+  res.end('Post operation not supported on dish '+req.params.dishId );
+});
+
+app.delete('/dishes/:dishId',(req, res, next)=>{
+  res.end('Deleting the dish '+ req.params.dishId);
+});
+
 
 app.use((req, res, next) => {
 	console.log(req.headers);
